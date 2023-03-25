a8238d4f10441c49688459bcd390feb3d3542a4a
JCurl08 <curl@student.ubc.ca>
2023-03-25 15:16:37 -0700

    Allowed custom brackets of size 8

--- brakcet-builder/src/LeftPanel/BracketLeftPanel.js
+++ brakcet-builder/src/LeftPanel/BracketLeftPanel.js
@@ -7,32 +7,74 @@ const rounds = [
     seeds: [
       {
         id: 1,
-        date: new Date().toDateString(),
-        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+        date: "",
+        teams: [{ name: 'As It Was - Harry Styles' }, { name: 'Unholy - Sam Smith ft. Kim Petras' }],
       },
       {
         id: 2,
-        date: new Date().toDateString(),
-        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+        date: "",
+        teams: [{ name: 'Anti-Hero - Taylor Swift' }, { name: 'Less Than Zero - The Weekend' }],
+      },
+      {
+        id: 3,
+        date: "",
+        teams: [{ name: 'Despecha - Rosalia' }, { name: 'Titi Me Pregunto - Bad Bunny' }],
+      },
+      {
+        id: 4,
+        date: "",
+        teams: [{ name: 'Free - Florence + The Machine' }, { name: 'Break My Soul - Beyonce' }],
       },
     ],
   },
   {
-    title: 'Round one',
+    title: 'Round two',
     seeds: [
       {
-        id: 3,
-        date: new Date().toDateString(),
-        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+        id: 5,
+        date: "",
+        teams: [{ name: "" }, { name: '' }],
+      },
+      {
+        id: 6,
+        date: "",
+        teams: [{ name: "" }, { name: '' }],
+      },
+    ],
+  },
+  {
+    title: 'Round three',
+    seeds: [
+      {
+        id: 7,
+        date: "",
+        teams: [{ name: "" }, { name: '' }],
+      },
+    ],
+  },
+  {
+    title: 'Winner',
+    seeds: [
+      {
+        id: 8,
+        date: "",
+        teams: [{ name: "" }],
       },
     ],
   },
 ];
 
 function BracketLeftPanel({input}) {
-  const generateBracket = () => {
+
+  
+  const generateBracket = (input) => {
+    for (let i = 0; i < 8; i += 2) {
+      rounds[0]["seeds"][i/2]["teams"][0]["name"] = input[i];
+      rounds[0]["seeds"][i/2]["teams"][1]["name"] = input[i+1];
+    }
     console.log("Bracket would be generated here.");
   }
+  generateBracket(input);
 
     return <div className="LeftPanel">
       <h1>SONGS PANEL</h1>
