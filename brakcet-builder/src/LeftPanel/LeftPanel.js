ae9c78081bb5b5a6be13c4f189290610f698def0
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-03-25 12:03:46 -0700

    Started form for LeftPanel
    Created some elements in the leftPanel form

--- brakcet-builder/src/LeftPanel/LeftPanel.js
+++ brakcet-builder/src/LeftPanel/LeftPanel.js
@@ -1,6 +1,36 @@
+import { useState } from "react"
+
 function LeftPanel() {
+  const [name, setName] = useState('')
+  const [textArea, setTextArea] = useState('')
+
+  const generateBracket = () => {
+    console.log("Bracket would be generated here.");
+  }
+
     return <div className="LeftPanel">
-        kkkkk
+      <h1>Enter form details here:</h1>
+      <div className="FormContainer">
+        <form onSubmit={generateBracket}>
+          <div>
+            <label>
+              Name:
+              <input type="text" value={name} onChange={({target}) => setName(target.value)}></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Input teams:
+              <textarea 
+                value={textArea} 
+                onChange={({target}) => setName(target.value)
+                }>
+                Separate teams by newlines
+              </textarea>
+            </label>
+          </div>
+        </form>
+      </div>
     </div>
 }
 
